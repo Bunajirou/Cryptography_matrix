f49b976ea8fd95b3f05fd984be709fafb414e5eb
Bunajirou <s2032052cz@s.chibakoudai.jp>
2021-06-27 19:56:37 +0900

    微修正

--- cryption_matrix.py
+++ cryption_matrix.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 from tkinter import messagebox
 import numpy as np
 import sympy
@@ -16,14 +16,15 @@ def create_key():
         d = sympy.randprime(min_prime, max_prime)
         det = (a*d - b*c) % n
         if(sympy.gcd(det, n) == 1):break
-    a_box.delete(0, tkinter.END)
-    b_box.delete(0, tkinter.END)
-    c_box.delete(0, tkinter.END)
-    d_box.delete(0, tkinter.END)
-    a_box.insert(tkinter.END, a)
-    b_box.insert(tkinter.END, b)
-    c_box.insert(tkinter.END, c)
-    d_box.insert(tkinter.END, d)
+    
+    a_box.delete(0, tk.END)
+    b_box.delete(0, tk.END)
+    c_box.delete(0, tk.END)
+    d_box.delete(0, tk.END)
+    a_box.insert(tk.END, a)
+    b_box.insert(tk.END, b)
+    c_box.insert(tk.END, c)
+    d_box.insert(tk.END, d)
 
 
 def encryption():
@@ -64,14 +65,13 @@ def encryption():
         c_txt_list.append(chr(i+A))
 
     c_txt = (''.join(c_txt_list))  # リスト内の文字を結合
-    output_box.delete(0, tkinter.END)
-    output_box.insert(tkinter.END, c_txt)
+    output_box.delete(0, tk.END)
+    output_box.insert(tk.END, c_txt)
 
 
 def decryption():
     A = 32
     n = 127-A
-
     try:
         a = int(a_box.get())
         b = int(b_box.get())
@@ -111,81 +111,77 @@ def decryption():
         p_txt_list.pop()
 
     p_txt = (''.join(p_txt_list))  # リスト内の文字を結合
-    output_box.delete(0, tkinter.END)
-    output_box.insert(tkinter.END, p_txt)
+    output_box.delete(0, tk.END)
+    output_box.insert(tk.END, p_txt)
 
 
 # ラジオボタンに応じて表示の変更
 def check_rdo():
     if(mode.get()==0):
-        run_button = tkinter.Button(text='暗号化実行', command=encryption)
+        run_button = tk.Button(text='暗号化実行', command=encryption)
         invar.set('<平文を入力>')
         outvar.set('<暗号文')
         keyvar.set('<暗号化鍵を入力>　または　<暗号化鍵を生成>')
     else:
-        run_button = tkinter.Button(text='復号化実行', command=decryption)
+        run_button = tk.Button(text='復号化実行', command=decryption)
         invar.set('<暗号文を入力>')
         outvar.set("<平文>")
         keyvar.set('<暗号化鍵を入力>')
     run_button.place(x=260, y=208)
-    input_box.delete(0, tkinter.END)
-    output_box.delete(0, tkinter.END)
+    input_box.delete(0, tk.END)
+    output_box.delete(0, tk.END)
 
 
 if __name__ == '__main__':
     # ウィンドウの作成
-    root = tkinter.Tk()
+    root = tk.Tk()
     root.title('行列を用いた暗号')
     root.geometry("350x300")
 
-    invar = tkinter.StringVar()
+    # 表示変更用制御変数
+    invar = tk.StringVar()
     invar.set('')
-    outvar = tkinter.StringVar()
+    outvar = tk.StringVar()
     outvar.set('')
-    keyvar = tkinter.StringVar()
+    keyvar = tk.StringVar()
     keyvar.set('')
 
-
     # ラベルの作成
-    key_label = tkinter.Label(textvariable=keyvar)
+    key_label = tk.Label(textvariable=keyvar)
     key_label.place(x=10, y=10)
-    bracket_label = tkinter.Label(text='(     )', font=('MS明朝', '50', 'normal'))
+    bracket_label = tk.Label(text='(     )', font=('MS明朝', '50', 'normal'))
     bracket_label.place(x=8, y=40)
-    mode_label = tkinter.Label(text='<モード選択>')
+    mode_label = tk.Label(text='<モード選択>')
     mode_label.place(x=10, y=120)
-    input_label = tkinter.Label(textvariable=invar)
+    input_label = tk.Label(textvariable=invar)
     input_label.place(x=10, y=190)
-    output_label = tkinter.Label(textvariable=outvar)
+    output_label = tk.Label(textvariable=outvar)
     output_label.place(x=10, y=240)
 
     # 入出力欄の作成
-    a_box = tkinter.Entry(width=5)
+    a_box = tk.Entry(width=5)
     a_box.place(x=40, y=55)
-    b_box = tkinter.Entry(width=5)
+    b_box = tk.Entry(width=5)
     b_box.place(x=90, y=55)
-    c_box = tkinter.Entry(width=5)
+    c_box = tk.Entry(width=5)
     c_box.place(x=40, y=80)
-    d_box = tkinter.Entry(width=5)
+    d_box = tk.Entry(width=5)
     d_box.place(x=90, y=80)
-    input_box = tkinter.Entry(width=40)
+    input_box = tk.Entry(width=40)
     input_box.place(x=10, y=211)
-    output_box = tkinter.Entry(width=40)
+    output_box = tk.Entry(width=40)
     output_box.place(x=10, y=261)
 
     # ボタンの作成
-    key_button = tkinter.Button(text='暗号化鍵を生成',command=create_key)
+    key_button = tk.Button(text='暗号化鍵を生成',command=create_key)
     key_button.place(x=205, y=53)
 
-    run_button = tkinter.Button(text='暗号化実行', command=encryption)
-    run_button.place(x=260, y=208)
-
     # ラジオボタンの作成
-    mode = tkinter.IntVar()
+    mode = tk.IntVar()
     mode.set(0)
-    de_rdo = tkinter.Radiobutton(root, value=0, variable=mode, text='暗号化モード', command=check_rdo)
+    de_rdo = tk.Radiobutton(root, value=0, variable=mode, text='暗号化モード', command=check_rdo)
     de_rdo.place(x=10, y=140)
-
-    en_rdo = tkinter.Radiobutton(root, value=1, variable=mode, text='復号化モード', command=check_rdo)
+    en_rdo = tk.Radiobutton(root, value=1, variable=mode, text='復号化モード', command=check_rdo)
     en_rdo.place(x=10, y=160)
 
     check_rdo()
